15b68b05ad7260d44aa5c800505892fc42671045
Meng Wang <mengwanguc@gmail.com>
2022-10-06 17:48:49 +0000

    correlated bursts

--- src/examples.sh
+++ src/examples.sh
@@ -4,6 +4,7 @@
 python main.py -sim_mode 6 -k_local 8 -p_local 2 -total_drives 2500 -drives_per_rack 50 -placement=RAID
 python burst.py -k_local 6 -p_local 1 -total_drives 21 -drives_per_rack 7 -placement=RAID
 python burst.py -k_local 6 -p_local 2 -total_drives 24 -drives_per_rack 8 -placement=RAID
+python burstBrute.py -k_net 3 -p_net 0 -k_local 6 -p_local 1
 
 # burst dp
 python main.py -sim_mode 6 -k_local 8 -p_local 2 -total_drives 2500 -drives_per_rack 50 -placement=DP
@@ -20,3 +21,6 @@ python burst.py -k_net 8 -p_net 2 -k_local 8 -p_local 2 -total_drives 2500 -driv
 python main.py -sim_mode 6 -io_speed 100 -cap 20 -k_net 8 -p_net 2 -k_local 8 -p_local 2 -total_drives 2500 -drives_per_rack 50 -placement=MLEC
 python burst.py -k_net 2 -p_net 1 -k_local 6 -p_local 1 -placement=MLEC
 python burst.py -k_net 2 -p_net 1 -k_local 6 -p_local 2 -placement=MLEC
+python burstBrute.py -k_net 2 -p_net 1 -k_local 6 -p_local 1
+python burst.py -k_net 8 -p_net 2 -k_local 8 -p_local 2 -total_drives 2500 -drives_per_rack 50 -placement=MLEC
+python burst.py -k_net 8 -p_net 2 -k_local 8 -p_local 2 -total_drives 400 -drives_per_rack 20 -placement=MLEC
