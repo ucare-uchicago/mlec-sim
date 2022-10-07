88e3dea14340772eb1e03f2ac72e6ad5d64ea829
Anjus George <40896699+Ann-Geo@users.noreply.github.com>
2022-10-07 10:36:44 -0400

    Removed print lines

--- LRC_decode/lrc_decode.py
+++ LRC_decode/lrc_decode.py
@@ -10,23 +10,23 @@ def calculate_recoverability(k, l, r, R):
     data_blocks = []
     for i in range(k):
         data_blocks.append('k'+str(i))
-    #print(data_blocks)
+
 
     #create l local parity blocks
     local_parities = []
     for i in range(l):
         local_parities.append('l'+str(i))
-    #print(local_parities)
+
 
     #create r global parity blocks
     global_parities = []
     for i in range(r):
         global_parities.append('r'+str(i))
-    #print(global_parities)
+
 
     #here is the whole set of data and parity blocks
     data_and_parity = data_blocks + local_parities + global_parities
-    #print(data_and_parity)
+
     
     #generate all R failure combinations from whole set of data and parity blocks
     all_comb = list(itertools.combinations(data_and_parity, R))
@@ -43,7 +43,6 @@ def calculate_recoverability(k, l, r, R):
     for i in range(start, end, step):
         x = i
         local_groups.append(data_blocks[x:x+step])
-    #print(local_groups)
     
     
     #setting initial recoverable patterns to 0
@@ -55,13 +54,13 @@ def calculate_recoverability(k, l, r, R):
         l_terms = l
         r_terms = r
         comb_list = list(all_comb[i])
-        #print(comb_list)
+
         
         #find number of data blocks in a failure combo/pattern
         for j in range(len(all_comb[i])):
             if 'k' in comb_list[j]:
                 k_terms = k_terms + 1
-        #print("   ", k_terms, l_terms, r_terms)
+
         
         #check if failed data block can be swapped with its local group parity
         #calculate the number of failed data blocks and available global parity blocks
@@ -71,19 +70,13 @@ def calculate_recoverability(k, l, r, R):
                     if all_comb[i][j] in local_groups[m]:
                         local_group_id = m
                         break
-                #print("   local_group_id = ", local_group_id)
                 if local_parities[local_group_id] not in comb_list:
                     k_terms = k_terms - 1
                     comb_list.append(local_parities[local_group_id])
             if 'r' in all_comb[i][j]:
                 r_terms = r_terms - 1
-        #print("   ", k_terms, r_terms)
         if k_terms <= r_terms:
             recoverable = recoverable + 1
-            #print(list(all_comb[i]), "-R")
-        #else:
-            #print(list(all_comb[i]), "-N")
-        #print("   recoverable = ", recoverable)
         
     return (recoverable/len(all_comb))
 
