878cce901f41bf51542603b36fe18734fff0d9fa
jiajunmao <aaronmao.hangzhou@gmail.com>
2022-11-03 16:36:57 -0700

    Update plotting script

--- jiajunm/plot.py
+++ jiajunm/plot.py
@@ -2,12 +2,11 @@ from cProfile import label
 import matplotlib.pyplot as plt
 import re
 
-from numpy import c_, mat
-
 if __name__ == "__main__":
     # Read from the log
     path = "src/s-result-DP_NET.log"
     c_path = "src/s-result-DP.log"
+    calc_path = "src/c-result-DP.log"
     
     k_net = []
     p_net = []
@@ -33,11 +32,7 @@ if __name__ == "__main__":
     
     c_afr = []
     c_nines = []
-    # for row in open(c_path, 'r'):
-    #     matcher = re.findall(r'(.*) (.*)', row)
-    #     (c_afr_, c_nines_) = matcher[0]
-    #     c_afr.append(float(c_afr_))
-    #     c_nines.append(float(c_nines_))
+    c_sigmas = []
         
     for row in open(c_path, 'r'):
         matcher = re.findall(r'\((.*)\+(.*)\)\((.*)\+(.*)\) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*) (.*)', row)
@@ -47,15 +42,34 @@ if __name__ == "__main__":
         # NET_DP uses local shard arguments
         c_nines.append(float(nines_))
         c_afr.append(float(afr_))
+        c_sigmas.append(float(sigma_))
 
-    print(c_afr)
-    print(c_nines)
-    print(afr)
-    print(nines)
-    plt.plot(c_afr, c_nines, label="Calculator")
-    plt.plot(afr, nines, label='Simulator')
+
+    calc_afr = []
+    calc_nines = []
+    
+    for row in open(calc_path, 'r'):
+        matcher = re.findall(r'(.*) (.*)', row)
+        (calc_afr_, calc_nines_) = matcher[0]
+        calc_afr.append(float(calc_afr_))
+        calc_nines.append(float(calc_nines_))
+    
+    # plt.plot(c_afr, c_nines, label="DP", color='blue')
+    # plt.plot(afr, nines, label='DP_NET')
+    
+    plt.plot(calc_afr, calc_nines, label="Web Calc")
+    
+    plt.errorbar(afr, nines, yerr=sigmas, label="DP_NET Sim")
+    plt.errorbar(c_afr, c_nines, yerr=c_sigmas, label="DP Sim")
+    
     plt.legend(loc="upper right")
+    
+    plt.ylim((0, 7))
+    plt.xlim((0,13))
+    
     plt.xlabel("Annual Failure Rate in %")
     plt.ylabel("Durability (Num of Nines)")
+    
     plt.title("Network DP Simulator vs Calculator")
     plt.show()
+    plt.savefig('plt.png')
