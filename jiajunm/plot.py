c403f34fdc84d70026779cb921a2ace8ec29dc3a
barbadosian <jiajunm@uchicago.edu>
2022-11-14 20:03:53 -0800

    Modify jiajunm parse and plot

--- jiajunm/plot.py
+++ jiajunm/plot.py
@@ -1,25 +1,12 @@
-from cProfile import label
-from parse import parse_sim_result, parse_calc_result
+from parse import parse_calc_result, parse_sim_result
 import matplotlib.pyplot as plt
-import re
 
 if __name__ == "__main__":
-    # Read from the log
-    calc_result = parse_calc_result("src/c-result-DP.log")
-    twenty_racks = parse_sim_result("src/s-result-DP_NET_1.log")
-    forty_racks = parse_sim_result("src/s-result-DP_NET_2.log")
-    ten_racks = parse_sim_result("src/s-result-DP_NET_3.log")
+    new_sim_result = parse_sim_result("src/s-result-RAID_new.log")
+    sim_result = parse_sim_result("src/s-result-RAID.log")
     
-    # plt.plot(c_afr, c_nines, label="DP", color='blue')
-    # plt.plot(afr, nines, label='DP_NET')
-    
-    print(calc_result['afr'])
-    print(calc_result['nines'])
-    # plt.plot(calc_result['afr'], calc_result['nines'], label="Web c")
-    
-    plt.errorbar(ten_racks['afr'], ten_racks['nines'], yerr=ten_racks['sigma'], label="DP Net 10 racks")
-    plt.errorbar(twenty_racks['afr'], twenty_racks['nines'], yerr=twenty_racks['sigma'], label="DP Net 20 racks")
-    plt.errorbar(forty_racks['afr'], forty_racks['nines'], yerr=forty_racks['sigma'], label="DP Net 40 rakcs")
+    plt.errorbar(new_sim_result['afr'], new_sim_result['nines'], yerr=new_sim_result['sigma'], label="New Sim DP")
+    plt.errorbar(sim_result['afr'], sim_result['nines'], yerr=sim_result['sigma'], label="Now Sim")
     # plt.errorbar(c_afr, c_nines, yerr=c_sigmas, label="DP Sim")
     
     plt.legend(loc="upper right")
@@ -30,6 +17,6 @@ if __name__ == "__main__":
     plt.xlabel("Annual Failure Rate in %")
     plt.ylabel("Durability (Num of Nines)")
     
-    plt.title("Network DP Simulator vs Calculator")
-    plt.show()
-    plt.savefig('plt.png')
+    plt.title("Restructured sim DP")
+    # plt.show()
+    plt.savefig('jiajunm/plt.png')
