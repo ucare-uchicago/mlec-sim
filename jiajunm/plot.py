3747033053b9c29516cb148818589567b425ddf7
barbadosian <jiajunm@uchicago.edu>
2022-11-14 22:06:09 -0800

    Modify jiajunm parse and plot

--- jiajunm/plot.py
+++ jiajunm/plot.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     new_sim_result = parse_sim_result("src/s-result-RAID_new.log")
     sim_result = parse_sim_result("src/s-result-RAID.log")
     
-    plt.errorbar(new_sim_result['afr'], new_sim_result['nines'], yerr=new_sim_result['sigma'], label="New Sim DP")
+    plt.errorbar(new_sim_result['afr'], new_sim_result['nines'], yerr=new_sim_result['sigma'], label="New Sim RAID")
     plt.errorbar(sim_result['afr'], sim_result['nines'], yerr=sim_result['sigma'], label="Now Sim")
     # plt.errorbar(c_afr, c_nines, yerr=c_sigmas, label="DP Sim")
     
