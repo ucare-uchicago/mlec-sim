8c59d1041fa78f695237525a20d7e7101f8a85cc
barbadosian <jiajunm@uchicago.edu>
2022-11-18 14:38:31 -0800

    Remove all logging

--- src/policies/netraid/repair.py
+++ src/policies/netraid/repair.py
@@ -13,11 +13,11 @@ from heapq import heappush
 
 def netraid_repair(state: State, repair_queue):
     for diskId in state.get_failed_disks():
-        logging.info("Trying to update repair for disk %s", diskId)
+        #logging.info("Trying to update repair for disk %s", diskId)
         rackId = diskId // state.sys.num_disks_per_rack
         # If the rack is normal AND the disk is not awaiting repair
         if state.racks[rackId].state == Rack.STATE_NORMAL and (diskId not in state.simulation.delay_repair_queue):
-            logging.info("Disk has healthy parent rack, and is not awaiting repair")
+            #logging.info("Disk has healthy parent rack, and is not awaiting repair")
             # logging.info("  update_repair_event. diskId: {}".format(diskId))
             repair_time = state.disks[diskId].repair_time[0]
             #-----------------------------------------------------
