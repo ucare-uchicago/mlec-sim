b9ae43fc57a43e35b9092131dea83f21c822d266
barbadosian <jiajunm@uchicago.edu>
2022-11-18 19:42:39 -0800

    Updating plot

--- jiajunm/plot.py
+++ jiajunm/plot.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     new_sim_result = parse_sim_result("src/logs/s-result-RAID_NET_new.log")
     sim_result = parse_sim_result("src/logs/s-result-RAID_NET.log")
     
-    plt.errorbar(new_sim_result['afr'], new_sim_result['nines'], yerr=new_sim_result['sigma'], label="New Sim RAID NET")
-    plt.errorbar(sim_result['afr'], sim_result['nines'], yerr=sim_result['sigma'], label="Now Sim")
+    plt.errorbar(new_sim_result['afr'], new_sim_result['nines'], yerr=new_sim_result['sigma'], label="Net RAID w/ Inf Network Bandwidth")
+    plt.errorbar(sim_result['afr'], sim_result['nines'], yerr=sim_result['sigma'], label="Old Sim")
     # plt.errorbar(c_afr, c_nines, yerr=c_sigmas, label="DP Sim")
     
     plt.legend(loc="upper right")
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     plt.xlabel("Annual Failure Rate in %")
     plt.ylabel("Durability (Num of Nines)")
     
-    plt.title("Restructured sim DP")
+    plt.title("Net RAID with inf Network Bandwidth Constraint")
     # plt.show()
     plt.savefig('jiajunm/plt.png')
