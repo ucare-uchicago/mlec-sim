2c42ef02b2dccbcff67345660a2358ed1da45d15
barbadosian <jiajunm@uchicago.edu>
2022-12-06 15:36:01 -0800

    Fix delay queue checking not checking in the list

--- src/policies/netraid/repair.py
+++ src/policies/netraid/repair.py
@@ -5,7 +5,7 @@ import logging
 if typing.TYPE_CHECKING:
     from state import State
 
-from components.network import Network
+from constants.Components import Components
 from components.disk import Disk
 from components.rack import Rack
 
@@ -16,7 +16,7 @@ def netraid_repair(state: State, repair_queue):
         #logging.info("Trying to update repair for disk %s", diskId)
         rackId = diskId // state.sys.num_disks_per_rack
         # If the rack is normal AND the disk is not awaiting repair
-        if state.racks[rackId].state == Rack.STATE_NORMAL and (diskId not in state.simulation.delay_repair_queue):
+        if state.racks[rackId].state == Rack.STATE_NORMAL and (diskId not in state.simulation.delay_repair_queue[Components.DISK]):
             #logging.info("Disk has healthy parent rack, and is not awaiting repair")
             # logging.info("  update_repair_event. diskId: {}".format(diskId))
             repair_time = state.disks[diskId].repair_time[0]
