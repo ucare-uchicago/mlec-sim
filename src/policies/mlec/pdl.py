90513ff34d9fa4db0894d02273b4164d09d04fe7
barbadosian <jiajunm@uchicago.edu>
2022-12-10 19:56:32 -0800

    Check whether diskId or dgId is in delay repair queue before updating repair time

--- src/policies/mlec/pdl.py
+++ src/policies/mlec/pdl.py
@@ -5,6 +5,6 @@ def mlec_cluster_pdl(state):
     for i in range(state.policy.num_diskgroup_stripesets):
         failed_diskgroups_per_stripeset = state.policy.get_failed_diskgroups_per_stripeset(i)
         if len(failed_diskgroups_per_stripeset) > state.sys.top_m:
-            logging.warn("System failure caused by diskgroup stripeset %s with failures %s", i, failed_diskgroups_per_stripeset)
+            # logging.warn("System failure caused by diskgroup stripeset %s with failures %s", i, failed_diskgroups_per_stripeset)
             prob = 1
     return prob
