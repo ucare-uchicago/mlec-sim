3a1e19653251fca6cd75bf22228abf6a268d36a9
Meng Wang <mengwanguc@gmail.com>
2023-03-11 15:48:55 +0000

    new layout for slec_local_dp

--- src/policies/slec_local_dp/layout.py
+++ src/policies/slec_local_dp/layout.py
@@ -4,8 +4,17 @@ import typing
 if typing.TYPE_CHECKING:
     from system import System
 
+from components.spool import Spool
+
 def slec_local_dp_layout(sys: System):
-    sys.flat_decluster_rack_layout = {}
-    for rackId in sys.rackIds:
-        disks_per_rack = sys.disks_per_rack[rackId]
-        sys.flat_decluster_rack_layout[rackId] = disks_per_rack
+    sys.spools = []
+    num_spools = sys.num_disks // sys.spool_size
+    num_spools_per_rack = sys.num_disks_per_rack // sys.spool_size
+
+    for spoolId in range(num_spools):
+        spool = Spool(spoolId, sys.spool_size)
+        spool.rackId = spoolId // num_spools_per_rack
+        spool.diskIds = range(spoolId*sys.spool_size, (spoolId+1)*sys.spool_size)
+        for diskId in spool.diskIds:
+            sys.disks[diskId].spoolId = spoolId
+        sys.spools.append(spool)
